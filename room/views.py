59c901af4547ab2ad85dd7790b445db1f0047552
P charan lenka <preetamlenka3@gmail.com>
2023-11-20 12:56:41 +0530

    Show list of Rooms

--- room/views.py
+++ room/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Room
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html',{'rooms':rooms})
